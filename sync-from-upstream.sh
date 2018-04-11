6f2e701f2e864304b74db20c6d6674866ac34ae5
Mu Lu <mlu163@bloomberg.net>
2018-04-12 00:00:43 +0900

    update sync script

--- sync-from-upstream.sh
+++ sync-from-upstream.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
-if [[ $USER == "murphy" ]];then
-    SYNC_TO=merge
-else
-    SYNC_TO=for_upstream_merge
-fi
-
+[ -z "$SYNC_TO" ] && SYNC_TO=master
 git show-branch $SYNC_TO > /dev/null 
 if [ $? -ne 0 ];then
     echo Cannot find local branch $SYNC_TO !
